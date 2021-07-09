846ba9f412db36a89a8df6ebd2e767cd6c6254e1
Flowsqy <Flo504.Dev@gmail.com>
2021-07-09 02:57:26 +0200

    Init project

---
+++ src/main/java/fr/flowsqy/dynamiccommand/DynamicCommandPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.dynamiccommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DynamicCommandPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
